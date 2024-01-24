5c8858120a6c1bfd87bceb011defc3bc19b5c64a
lambor590 <lambor590@gmail.com>
2024-01-24 20:45:53 +0100

    Optimizaciones de los endpoints de las cuentas

--- routes/main/cuentas.js
+++ routes/main/cuentas.js
@@ -1,7 +1,6 @@
 // Importamos dependencias
 const express = require("express");
 const axios = require("axios");
-
 // Importamos middlewares
 const auth = require("../../middleware/auth");
 const { admin, viewer, self } = require("../../middleware/roles");
@@ -32,17 +31,16 @@ router.get("/", [auth, viewer], (req, res) => {
 router.get("/nombre=:nombre&tag=:tag", [auth, viewer], (req, res) => {
     // GET /cuentas/nombre=:nombre
     // recibimos el nombre de invocador a partir de su nombre
-    const nombre = req.params.nombre;
-    const tag = req.params.tag;
+    const { nombre, tag } = req.params;
 
-    axios.get("https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/" + nombre + "/" + tag + "?api_key=" + RIOT_API).then((cuentaRiot) => {
-        if (cuentaRiot.data["puuid"] != null && cuentaRiot.data["gameName"] != null) {
+    axios.get(`https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${nombre}/${tag}?api_key=${RIOT_API}`).then((cuentaRiot) => {
+        if (cuentaRiot.data.puuid && cuentaRiot.data.gameName) {
             const sqlSelect = "SELECT invocador, tag, puuid_lol FROM cuentas_lol WHERE invocador = ? AND tag = ?";
             db.query(sqlSelect, [nombre, tag], (err, result) => {
                 if (err) {
                     res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
                 } else {
-                    if (result.length == 0) {
+                    if (result.length === 0) {
                         res.send({ status: 200, success: true, result: cuentaRiot.data, existe: false });
                     } else {
                         res.send({ status: 200, success: true, result: result, existe: true });
@@ -58,16 +56,10 @@ router.get("/nombre=:nombre&tag=:tag", [auth, viewer], (req, res) => {
 router.post("/", [auth, self], async (req, res) => {
     // POST /cuentas
     // creamos un usuario
-    idusuario = req.body.id_usuario;
-    invocador = req.body.invocador;
-    tag = req.body.tag;
-    puuid = req.body.puuid;
-    lineaprincipal = req.body.linea_principal;
-    lineasecundaria = req.body.linea_secundaria;
+    const { id_usuario, invocador, tag, puuid, lineaprincipal, lineasecundaria } = req.body;
 
-    const sql =
-        "INSERT INTO `cuentas_lol` (`id_cuenta`, `id_usuario`, `id_juego`, `invocador`, `tag`, `puuid_lol`, `linea_principal`, `linea_secundaria`) VALUES (NULL, ?, 1, ?, ?, ?, ?, ?)";
-    db.query(sql, [idusuario, invocador, tag, puuid, lineaprincipal, lineasecundaria], (err, result) => {
+    const sql = "INSERT INTO `cuentas_lol` (`id_cuenta`, `id_usuario`, `id_juego`, `invocador`, `tag`, `puuid_lol`, `linea_principal`, `linea_secundaria`) VALUES (NULL, ?, 1, ?, ?, ?, ?, ?)";
+    db.query(sql, [id_usuario, invocador, tag, puuid, lineaprincipal, lineasecundaria], (err, result) => {
         if (err) {
             res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
         } else {
@@ -79,12 +71,7 @@ router.post("/", [auth, self], async (req, res) => {
 router.put("/", [auth, self], async (req, res) => {
     // PUT /cuentas
     // modificamos una cuenta de un usuario
-    id = req.body.id;
-    invocador = req.body.invocador;
-    tag = req.body.tag;
-    puuid_lol = req.body.puuid_lol;
-    linea_principal = req.body.linea_principal;
-    linea_secundaria = req.body.linea_secundaria;
+    const { id, invocador, tag, puuid_lol, linea_principal, linea_secundaria } = req.body;
 
     const sql = "UPDATE cuentas_lol SET invocador = ?, puuid_lol = ?, linea_principal = ?, linea_secundaria = ?, tag = ? WHERE id_cuenta = ?";
     db.query(sql, [invocador, puuid_lol, linea_principal, linea_secundaria, tag, id], (err, result) => {
@@ -99,7 +86,7 @@ router.put("/", [auth, self], async (req, res) => {
 router.delete("/", [auth, self], async (req, res) => {
     // DELETE /cuentas
     // eliminamos una cuenta a partir de su id
-    id_cuenta = req.body.id_cuenta;
+    const { id_cuenta } = req.body;
 
     const sqlDelete = "DELETE FROM cuentas_lol WHERE id_cuenta = ?";
     db.query(sqlDelete, [id_cuenta], (err, result) => {
