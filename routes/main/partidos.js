c3c0deb060224ed911192bcf6a1c15dd28d56a5b
lambor590 <lambor590@gmail.com>
2024-01-26 23:19:20 +0100

    Optimizar /partidos

--- routes/main/partidos.js
+++ routes/main/partidos.js
@@ -7,23 +7,17 @@ const auth = require("../../middleware/auth");
 const { admin, viewer, self } = require("../../middleware/roles");
 const db = require("../../middleware/db");
 
-//Importtamos utils
+//Importamos utils
 const returnPlayer = require("../../utils/returnPlayer");
 const { getPlayerStats } = require("../../utils/getPlayerStats");
 
 // Set del router
 const router = express.Router();
-
 const RIOT_API = "RGAPI-48c2e07c-b903-4720-be64-d3ba9a416206";
 
-// *************************
-// Set up the route handlers
-// *************************
-
 router.get("/", (req, res) => {
-  // /usuarios
-  // recibimos todos los usuarios
-
+  // GET /partidos
+  // recibimos todos los partidos
   const sqlSelect = "SELECT * FROM partidos WHERE tipo = 0";
   db.query(sqlSelect, (err, result) => {
     if (err) {
@@ -34,10 +28,22 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get("/inhouses", [auth, viewer], (req, res) => {
-  // /usuarios
-  // recibimos todos los usuarios
+router.get("/partidos", [auth, viewer], (req, res) => {
+  // GET /partidos/partidos
+  // recibimos todos los partidos
+  const sqlSelect = "SELECT * FROM partidos ORDER BY fecha DESC LIMIT 5 WHERE tipo = 0";
+  db.query(sqlSelect, (err, result) => {
+    if (err) {
+      res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
+    } else {
+      res.send({ status: 200, success: true, result: result });
+    }
+  });
+});
 
+router.get("/inhouses", [auth, viewer], (req, res) => {
+  // GET /partidos/inhouses
+  // recibimos todas las inhouses ordenadas por progreso y fecha
   const sqlSelect = "SELECT * FROM partidos WHERE tipo = 1 ORDER BY progreso ASC, fecha ASC";
   db.query(sqlSelect, (err, result) => {
     if (err) {
@@ -49,10 +55,9 @@ router.get("/inhouses", [auth, viewer], (req, res) => {
 });
 
 router.get("/inhouses/id=:id", [auth, viewer], (req, res) => {
-  // /usuarios
-  // recibimos todos los usuarios
+  // GET /partidos/inhouses/id=:id
+  // recibimos todas las inhouses ordenadas por progreso y fecha basadas en el id
   const id = req.params.id;
-
   const sqlSelect = "SELECT * FROM partidos WHERE id_partido = ? AND tipo = 1 ORDER BY progreso ASC, fecha ASC";
   db.query(sqlSelect, [id], (err, result) => {
     if (err) {
@@ -64,6 +69,7 @@ router.get("/inhouses/id=:id", [auth, viewer], (req, res) => {
 });
 
 router.put("/inhouses/estadisticas", (req, res) => {
+  // PUT /partidos/inhouses/estadisticas
   const sqlSelect = "SELECT match_id FROM partidos WHERE tipo = 1 AND estadisticas_recogidas = 0 AND match_id IS NOT null limit 1";
   db.query(sqlSelect, (err, result) => {
     if (err) {
@@ -75,19 +81,16 @@ router.put("/inhouses/estadisticas", (req, res) => {
 });
 
 router.get("/id=:id", [auth, viewer], (req, res) => {
-  // /usuarios/id=:id
+  // GET /partidos/id=:id
   // recibimos usuario por id
   const id = req.params.id;
-
   returnPlayer(id, res);
 });
 
 router.post("/inhouses", [auth, admin], async (req, res) => {
-  // /crearusuario
-  // crearmos un usuario
-  fecha = req.body.fecha;
-  //
-
+  // POST /partidos/inhouses
+  // creamos una inhouse
+  const fecha = req.body.fecha;
   axios
     .post("https://americas.api.riotgames.com/lol/tournament/v5/codes?tournamentId=7188490&count=1&api_key=" + RIOT_API, {
       mapType: "SUMMONERS_RIFT",
@@ -96,7 +99,6 @@ router.post("/inhouses", [auth, admin], async (req, res) => {
       teamSize: 5,
     })
     .then((response) => {
-      //console.log(response.data)
       const sql = "INSERT INTO partidos (tipo, fecha, codigo_torneo) VALUES (1, ?, ?)";
       db.query(sql, [fecha, response.data], (err, result) => {
         if (err) {
@@ -109,83 +111,76 @@ router.post("/inhouses", [auth, admin], async (req, res) => {
 });
 
 router.put("/inhouses", [auth, self], async (req, res) => {
-  // /partidos/inhouses
+  // PUT /partidos/inhouses
   // inscripcion de jugador en inhouse
-  id_inhouse = req.body.id_inhouse;
-  id_usuario = req.body.id_usuario;
-  posicion = req.body.posicion;
-  side = req.body.side;
-  //
+  const { id_inhouse, id_usuario, posicion, side } = req.body;
   let existe = false;
 
-  let sqlSelect = "";
-  if (side == 1 || side == 2) {
-    sqlSelect = "SELECT jugadores_blue, jugadores_red FROM partidos WHERE id_partido = ?";
-  } else {
-    res.send({ status: 400, success: false, reason: "Problema con la información recibida.", error: err });
+  if (side !== 1 && side !== 2) {
+    res.send({ status: 400, success: false, reason: "Problema con la información recibida." });
+    return;
   }
+
+  const sqlSelect = "SELECT jugadores_blue, jugadores_red FROM partidos WHERE id_partido = ?";
   db.query(sqlSelect, [id_inhouse], (err, result) => {
     if (err) {
       res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-    } else {
-      if (side == 1 || side == 2) {
-        for (let i = 0; i < JSON.parse(result[0]["jugadores_blue"]).length; i++) {
-          if (JSON.parse(result[0]["jugadores_blue"])[i].id === id_usuario) {
-            existe = true;
-            break;
-          }
-        }
-        for (let i = 0; i < JSON.parse(result[0]["jugadores_red"]).length; i++) {
-          if (JSON.parse(result[0]["jugadores_red"])[i].id === id_usuario) {
-            existe = true;
-            break;
-          }
-        }
-        if (existe) {
-          res.send({ status: 200, success: false, result: "El usuario ya existe." });
-        } else {
-          let sqlUpdate = "";
-          if (side == 1) {
-            sqlUpdate = "UPDATE partidos SET jugadores_blue = ? WHERE id_partido = ?;";
-            JSON.parse(result[0]["jugadores_blue"])[posicion]["id"] = id_usuario;
-            let JSONFinalizado = JSON.parse(result[0]["jugadores_blue"]);
-            JSONFinalizado[posicion]["id"] = id_usuario;
-            db.query(sqlUpdate, [JSON.stringify(JSONFinalizado), id_inhouse], (err, result) => {
-              if (err) {
-                res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-              } else {
-                res.send({ status: 200, success: true, result: result });
-              }
-            });
-          } else if (side == 2) {
-            sqlUpdate = "UPDATE partidos SET jugadores_red = ? WHERE id_partido = ?;";
-            JSON.parse(result[0]["jugadores_red"])[posicion]["id"] = id_usuario;
-            let JSONFinalizado = JSON.parse(result[0]["jugadores_red"]);
-            JSONFinalizado[posicion]["id"] = id_usuario;
-            db.query(sqlUpdate, [JSON.stringify(JSONFinalizado), id_inhouse], (err, result) => {
-              if (err) {
-                res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-              } else {
-                res.send({ status: 200, success: true, result: result });
-              }
-            });
-          } else {
-            res.send({ status: 400, success: false, reason: "Problema con la información recibida.", error: err });
-          }
-        }
-      } else {
-        res.send({ status: 400, success: false, reason: "Problema con la información recibida.", error: err });
+      return;
+    }
+
+    const jugadoresBlue = JSON.parse(result[0]["jugadores_blue"]);
+    const jugadoresRed = JSON.parse(result[0]["jugadores_red"]);
+
+    for (let i = 0; i < jugadoresBlue.length; i++) {
+      if (jugadoresBlue[i].id === id_usuario) {
+        existe = true;
+        break;
       }
+    }
+
+    if (!existe) {
+      for (let i = 0; i < jugadoresRed.length; i++) {
+        if (jugadoresRed[i].id === id_usuario) {
+          existe = true;
+          break;
+        }
+      }
+    }
+
+    if (existe) {
+      res.send({ status: 200, success: false, result: "El usuario ya existe." });
+    } else {
+      let sqlUpdate = "";
+      let JSONFinalizado = [];
+
+      if (side === 1) {
+        sqlUpdate = "UPDATE partidos SET jugadores_blue = ? WHERE id_partido = ?";
+        jugadoresBlue[posicion]["id"] = id_usuario;
+        JSONFinalizado = jugadoresBlue;
+      } else if (side === 2) {
+        sqlUpdate = "UPDATE partidos SET jugadores_red = ? WHERE id_partido = ?";
+        jugadoresRed[posicion]["id"] = id_usuario;
+        JSONFinalizado = jugadoresRed;
+      } else {
+        res.send({ status: 400, success: false, reason: "Problema con la información recibida." });
+        return;
+      }
+
+      db.query(sqlUpdate, [JSON.stringify(JSONFinalizado), id_inhouse], (err, result) => {
+        if (err) {
+          res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
+        } else {
+          res.send({ status: 200, success: true, result: result });
+        }
+      });
     }
   });
 });
 
 router.put("/", [auth, admin], async (req, res) => {
-  // /modificarusuario
-  // modificamos un usuario
-  id_usuario = req.body.id_usuario;
-  columna = req.body.columna;
-  valor = req.body.valor;
+  // PUT /partidos
+  // modificamos un partido
+  const { id_usuario, columna, valor } = req.body;
 
   const sql = "UPDATE usuarios SET `" + columna + "` = ? WHERE id_usuario = ?";
   db.query(sql, [valor, id_usuario], (err, result) => {
@@ -198,40 +193,25 @@ router.put("/", [auth, admin], async (req, res) => {
 });
 
 router.delete("/", [auth, admin], async (req, res) => {
-  // /borrarusuario
-  // eliminamos un usuario a partir de su id
-  id = req.body.id;
+  // DELETE /partidos
+  // borramos un partido
+  const id = req.body.id;
 
   const sqlDeleteLogs = "DELETE FROM logs WHERE id_usuario = ?";
   const sqlDeleteSesiones = "DELETE FROM sesiones WHERE id_usuario = ?";
   const sqlDeleteCuentas = "DELETE FROM cuentas_lol WHERE id_usuario = ?";
   const sqlDeleteUsuario = "DELETE FROM usuarios WHERE id_usuario = ?";
-  db.query(sqlDeleteLogs, [id], (err, result) => {
-    if (err) {
-      res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-    } else {
-      db.query(sqlDeleteSesiones, [id], (err, result) => {
-        if (err) {
-          res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-        } else {
-          db.query(sqlDeleteCuentas, [id], (err, result) => {
-            if (err) {
-              res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-            } else {
-              db.query(sqlDeleteUsuario, [id], (err, result) => {
-                if (err) {
-                  res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-                } else {
-                  res.send({ status: 200, success: true, result: result });
-                }
-              });
-            }
-          });
-        }
-      });
-    }
-  });
+
+  try {
+    db.query(sqlDeleteLogs, [id]);
+    db.query(sqlDeleteSesiones, [id]);
+    db.query(sqlDeleteCuentas, [id]);
+    db.query(sqlDeleteUsuario, [id]);
+
+    res.send({ status: 200, success: true });
+  } catch (err) {
+    res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
+  }
 });
 
-// Exportamos el router
 module.exports = router;
