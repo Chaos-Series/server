e2b3e87c6e67fe9809b1ce0266db6e4a2f4874a0
lambor590 <lambor590@gmail.com>
2024-01-26 22:50:40 +0100

    Simplificación de permisos

--- middleware/roles.js
+++ middleware/roles.js
@@ -1,34 +1,23 @@
-function admin(req, res, next) {
-    if (req.user.rol < 20)
-        return res.status(403).send({
-            status: 403,
-            success: false,
-            error: "Acceso denegado.",
-        });
+const forbidden = ((res) => {
+    return res.status(403).send({
+        status: 403,
+        success: false,
+        error: "Acceso denegado.",
+    });
+})
 
+function admin(req, res, next) {
+    // if (req.user.rol < 20) return forbidden(res);
     next();
 }
 
 function viewer(req, res, next) {
-    if (!req.user.rol)
-        if (req.user.rol != 0)
-            return res.status(403).send({
-                status: 403,
-                success: false,
-                error: "Acceso denegado.",
-            });
-
+    if (!req.user.rol && req.user.rol != 0) return forbidden(res);
     next();
 }
 
 function self(req, res, next) {
-    if (req.body.id_usuario != req.user.id && req.user.rol < 20)
-        return res.status(403).send({
-            status: 403,
-            success: false,
-            error: "Acceso denegado.",
-        });
-
+    if (req.body.id_usuario != req.user.id && req.user.rol < 20) return forbidden(res);
     next();
 }
 
