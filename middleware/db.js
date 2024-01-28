b949522381b345abbbbfe53cf7c96ad90fa8660f
Zas <thezasm03@gmail.com>
2024-01-28 20:07:32 +0100

    Mejoras .env

--- middleware/db.js
+++ middleware/db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql");
 
+console.log(process.env.DB_HOST);
 const db = mysql.createPool({
     //crear la conexion a la base de datos
     host: process.env.DB_HOST,
     user: process.env.DB_USER_PROD,
-    password: process.env.DB_PASS_PROD,
-    database: process.env.DATABASE_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_DATABASE_PROD,
 });
 
 module.exports = db;
