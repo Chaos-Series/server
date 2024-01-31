b5622a79527baa883c70dbf0f2b3d3ed453c6e1b
lambor590 <lambor590@gmail.com>
2024-01-31 19:10:19 +0100

    Usar nueva función de queries y simplificar

--- utils/returnPlayer.js
+++ utils/returnPlayer.js
@@ -1,16 +1,12 @@
 const db = require("../middleware/db");
+const { returnQuery } = require("./returnQuery");
 
 async function returnPlayerList(res) {
-    const sqlInfo = "SELECT id_usuario, id_equipo, nombre_usuario, apellido_usuario, nick_usuario, icono, circuitotormenta, twitter, discord FROM usuarios WHERE rol = 1 AND nombre_usuario != 'NECESITA MODIFICACIÓN' AND apellido_usuario != 'NECESITA MODIFICACIÓN'";
-    const sqlCuentas = "SELECT * FROM cuentas_lol";
-    const sqlEquipo = "SELECT * FROM equipos";
-    const sqlEstadisticas = "SELECT * FROM estadisticas_usuarios";
-
     try {
-        let usuarios = await query(sqlInfo);
-        let cuentas = await query(sqlCuentas);
-        let equipos = await query(sqlEquipo);
-        let estadisticas = await query(sqlEstadisticas);
+        let usuarios = await returnQuery("SELECT id_usuario, id_equipo, nombre_usuario, apellido_usuario, nick_usuario, icono, circuitotormenta, twitter, discord FROM usuarios WHERE rol = 1 AND nombre_usuario != 'NECESITA MODIFICACIÓN' AND apellido_usuario != 'NECESITA MODIFICACIÓN'", res, [], false);
+        let cuentas = await returnQuery("SELECT * FROM cuentas_lol", res, [], false);
+        let equipos = await returnQuery("SELECT * FROM equipos", res, [], false);
+        let estadisticas = await returnQuery("SELECT * FROM estadisticas_usuarios", res, [], false);
 
         usuarios = usuarios.filter((info) => {
             let equipo = equipos.find((equipo) => equipo.id_equipo == info.id_equipo);
@@ -31,18 +27,14 @@ async function returnPlayerList(res) {
 }
 
 async function returnPlayer(id, res) {
-    const sqlUser = "SELECT id_usuario, id_equipo, id_discord, nombre_usuario, apellido_usuario, nick_usuario, edad, rol, icono, usuario_activado, circuitotormenta, twitter, discord FROM usuarios WHERE id_usuario = ?";
-    const sqlEquipo = "SELECT * FROM equipos WHERE id_equipo = ?";
-    const sqlCuentas = "SELECT * FROM cuentas_lol WHERE id_usuario = ?";
-
     try {
         let usuario = { info: {}, equipo: {}, cuentas: {}, estadisticas: {} };
 
-        usuario.info = (await query(sqlUser, [id]))[0];
-        usuario.cuentas = await query(sqlCuentas, [id]);
+        usuario.info = (await returnQuery("SELECT id_usuario, id_equipo, id_discord, nombre_usuario, apellido_usuario, nick_usuario, edad, rol, icono, usuario_activado, circuitotormenta, twitter, discord FROM usuarios WHERE id_usuario = ?", res, [id], false))[0];
+        usuario.cuentas = await returnQuery("SELECT * FROM cuentas_lol WHERE id_usuario = ?", res, [id], false);
 
         if (usuario.info["id_equipo"]) {
-            usuario.equipo = (await query(sqlEquipo, usuario.info["id_equipo"]))[0];
+            usuario.equipo = (await returnQuery("SELECT * FROM equipos WHERE id_equipo = ?", res, usuario.info["id_equipo"], false))[0];
         }
 
         res.send({ status: 200, success: true, result: usuario });
@@ -51,16 +43,4 @@ async function returnPlayer(id, res) {
     }
 }
 
-function query(sql, params) {
-    return new Promise((resolve, reject) => {
-        db.query(sql, params, (error, result) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(result);
-            }
-        });
-    });
-}
-
-module.exports = { returnPlayer, returnPlayerList, query };
+module.exports = { returnPlayer, returnPlayerList };
