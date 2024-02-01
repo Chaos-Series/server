10316ffa7b0cab37c990f9d19571e334f01267b8
lambor590 <lambor590@gmail.com>
2024-02-01 20:55:43 +0100

    Optimizar rutas de clasificación y agregar consulta por ID

--- routes/main/clasificacion.js
+++ routes/main/clasificacion.js
@@ -18,9 +18,40 @@ const router = express.Router();
  * @returns {object} La clasificación de todos los equipos.
  */
 router.get("/", (req, res) => {
-    const { id_liga, id_temporada } = req.query;
+    const { query, params } = getSQL(req, "SELECT id_equipo, id_liga, id_temporada, victorias, derrotas, puntuacion FROM estadisticasequipos");
+    returnQuery(query, res, params);
+});
 
-    let query = "SELECT id_equipo, id_liga, id_temporada, victorias, derrotas, puntuacion FROM estadisticasequipos";
+
+/**
+ * Obtiene la clasificación de un equipo por su ID.
+ * 
+ * @route GET /clasificacion/id=:id
+ * 
+ * @returns {object} La clasificación de todos los equipos.
+ */
+router.get("/id=:id", (req, res) => {
+    const { query, params } = getSQL(req, "SELECT id_equipo, id_liga, id_temporada, victorias, derrotas, puntuacion FROM estadisticasequipos");
+    returnQuery(query, res, params);
+});
+
+
+/**
+ * Obtiene la clasificación de un equipo con estadísticas.
+ * 
+ * @route GET /clasificacion
+ * 
+ * @returns {object} La clasificación del equipo con estadísticas.
+*/
+router.get("/detalles/id=:id", (req, res) => {
+    const { query, params } = getSQL(req, "SELECT id_equipo, id_liga, id_temporada, victorias, derrotas, puntuacion, kda, oro, minions, barones, dragones, almas, torres, campeones_jugados, campeones_baneados FROM estadisticasequipos ORDER BY puntuacion DESC, id_equipo DESC");
+    returnQuery(query, res, params);
+});
+
+
+function getSQL(req, baseQuery) {
+    const { id_liga, id_temporada } = req.query;
+    let query = baseQuery;
     let params = [];
 
     if (id_liga && id_temporada) {
@@ -30,20 +61,8 @@ router.get("/", (req, res) => {
 
     query += " ORDER BY puntuacion DESC, id_equipo DESC";
 
-    returnQuery(query, res, params);
-});
-
-
-/**
- * Obtiene la clasificación de todos los equipos con estadísticas.
- * 
- * @route GET /clasificacion
- * 
- * @returns {object} La clasificación de todos los equipos con estadísticas.
-*/
-router.get("/detalles", (req, res) => {
-    returnQuery("SELECT id_equipo, id_liga, id_temporada, victorias, derrotas, puntuacion, kda, oro, minions, barones, dragones, almas, torres, campeones_jugados, campeones_baneados FROM estadisticasequipos ORDER BY puntuacion DESC, id_equipo DESC", res);
-});
+    return { query, params };
+}
 
 
 // Exportamos el router
