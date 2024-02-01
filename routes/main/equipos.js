6fd4b5448070d6d11b2868e1c31eddb5f2c8e800
lambor590 <lambor590@gmail.com>
2024-02-01 19:54:54 +0100

    No devolver equipo predeterminado + optimizaciones

--- routes/main/equipos.js
+++ routes/main/equipos.js
@@ -5,8 +5,8 @@ const path = require("path");
 
 // Importamos middlewares
 const auth = require("../../middleware/auth");
-const { admin, viewer } = require("../../middleware/roles");
-const db = require("../../middleware/db");
+const { admin } = require("../../middleware/roles");
+const { returnQuery } = require("../../utils/returnQuery");
 
 // Creamos el storage
 const storage = multer.diskStorage({
@@ -32,14 +32,14 @@ const router = express.Router();
  * @returns {object} Todos los equipos.
  */
 router.get("/", (req, res) => {
-    const sqlSelect = "SELECT * FROM equipos";
-    db.query(sqlSelect, (err, result) => {
-        if (err) {
+    returnQuery("SELECT * FROM equipos", res, [], false)
+        .then((result) => {
+            result.shift();
+            res.send({ status: 200, success: true, result: result });
+        })
+        .catch((err) => {
             res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-        } else {
-            res.send({ status: 200, success: true, result });
-        }
-    });
+        });
 });
 
 
@@ -53,15 +53,7 @@ router.get("/", (req, res) => {
  */
 router.get("/id=:id", (req, res) => {
     const id = req.params.id;
-    const sqlSelect =
-        "SELECT * FROM equipos LEFT JOIN ligas ON equipos.id_liga = ligas.id_liga LEFT JOIN temporadas ON equipos.id_temporada = temporadas.id_temporada WHERE equipos.id_equipo = ?";
-    db.query(sqlSelect, [id], (err, result) => {
-        if (err) {
-            res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-        } else {
-            res.send({ status: 200, success: true, result });
-        }
-    });
+    returnQuery("SELECT * FROM equipos LEFT JOIN ligas ON equipos.id_liga = ligas.id_liga LEFT JOIN temporadas ON equipos.id_temporada = temporadas.id_temporada WHERE equipos.id_equipo = ?", res, [id])
 });
 
 
@@ -75,15 +67,7 @@ router.get("/id=:id", (req, res) => {
  */
 router.get("/nombre=:nombre", (req, res) => {
     const nombre = req.params.nombre;
-    const sqlSelect =
-        "SELECT * FROM equipos LEFT JOIN ligas ON equipos.id_liga = ligas.id_liga LEFT JOIN temporadas ON equipos.id_temporada = temporadas.id_temporada WHERE equipos.nombre_equipo = ?";
-    db.query(sqlSelect, [nombre], (err, result) => {
-        if (err) {
-            res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-        } else {
-            res.send({ status: 200, success: true, result: result });
-        }
-    });
+    returnQuery("SELECT * FROM equipos LEFT JOIN ligas ON equipos.id_liga = ligas.id_liga LEFT JOIN temporadas ON equipos.id_temporada = temporadas.id_temporada WHERE equipos.nombre_equipo = ?", res, [nombre]);
 });
 
 
@@ -97,14 +81,21 @@ router.get("/nombre=:nombre", (req, res) => {
  */
 router.get("/usuarios/id=:id", (req, res) => {
     const id = req.params.id;
-    const sqlSelect = "SELECT id_usuario, id_equipo, id_discord, nombre_usuario, apellido_usuario, nick_usuario, edad, rol, icono, usuario_activado, circuitotormenta, twitter, discord FROM usuarios WHERE id_equipo = ?";
-    db.query(sqlSelect, [id], (err, result) => {
-        if (err) {
-            res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-        } else {
-            res.send({ status: 200, success: true, result: result });
-        }
-    });
+    returnQuery("SELECT id_usuario, id_equipo, id_discord, nombre_usuario, apellido_usuario, nick_usuario, edad, rol, icono, usuario_activado, circuitotormenta, twitter, discord FROM usuarios WHERE id_equipo = ?", res, [id]);
+});
+
+
+/**
+ * Obtiene todos los partidos de un equipo a partir de su ID.
+ * 
+ * @route GET /equipos/partidos/id=:id
+ * 
+ * @param {string} id - El ID del equipo.
+ * @returns {object} Todos los partidos de un equipo a partir de su ID.
+ */
+router.get("/partidos/id=:id", (req, res) => {
+    const id = req.params.id;
+    returnQuery("SELECT * FROM partidos WHERE tipo = 0 AND id_equipo1 = ? OR id_equipo2 = ? ORDER BY fecha", res, [id, id]);
 });
 
 
@@ -121,14 +112,7 @@ router.get("/usuarios/id=:id", (req, res) => {
 router.post("/", [auth, admin], upload.single("imagenEquipo"), async (req, res) => {
     const image = req.file;
     const { nombre, acronimo } = req.body;
-    const sql = "INSERT INTO `equipos` (`nombre_equipo`, `logo_equipo`, `acronimo_equipo`) VALUES (?, ?, ?)";
-    db.query(sql, [nombre, image.filename, acronimo], (err, result) => {
-        if (err) {
-            res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-        } else {
-            res.send({ status: 200, success: true, result: result });
-        }
-    });
+    returnQuery("INSERT INTO `equipos` (`nombre_equipo`, `logo_equipo`, `acronimo_equipo`) VALUES (?, ?, ?)", res, [nombre, image.filename, acronimo]);
 });
 
 
@@ -144,14 +128,7 @@ router.post("/", [auth, admin], upload.single("imagenEquipo"), async (req, res) 
  */
 router.put("/", [auth, admin], async (req, res) => {
     const { id, columna, valor } = req.body;
-    const sql = "UPDATE equipos SET `" + columna + "` = ? WHERE id_equipo = ?";
-    db.query(sql, [valor, id], (err, result) => {
-        if (err) {
-            res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-        } else {
-            res.send({ status: 200, success: true, result: result });
-        }
-    });
+    returnQuery("UPDATE equipos SET `" + columna + "` = ? WHERE id_equipo = ?", res, [valor, id])
 });
 
 
@@ -165,14 +142,7 @@ router.put("/", [auth, admin], async (req, res) => {
  */
 router.delete("/", [auth, admin], async (req, res) => {
     const id = req.body.id;
-    const sql = "DELETE FROM equipos WHERE id_equipo = ?";
-    db.query(sql, [id], (err, result) => {
-        if (err) {
-            res.send({ status: 500, success: false, reason: "Problema con la base de datos.", error: err });
-        } else {
-            res.send({ status: 200, success: true, result: result });
-        }
-    });
+    returnQuery("DELETE FROM equipos WHERE id_equipo = ?", res, [id])
 });
 
 module.exports = router;
